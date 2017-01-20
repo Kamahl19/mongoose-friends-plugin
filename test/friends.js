36d78f574b195ef7f0ba2033126af2909d28036c
Martin Litvaj <kamahl19@gmail.com>
2017-01-20 19:40:54 +0100

    use promises instead of callbacks

--- test/friends.js
+++ test/friends.js
@@ -1,13 +1,14 @@
 const mongoose = require('mongoose');
 const friendsPlugin = require('../');
 const Status = friendsPlugin.Status;
-const async = require('async');
 const assert = require('assert');
 const should = require('should');
 
 require('babel-polyfill');
 
 require('dotenv').config();
+
+mongoose.Promise = global.Promise;
 
 mongoose.connect(process.env.MONGODB_TEST);
 
@@ -26,132 +27,137 @@ suite('friends', function() {
   let u1;
   let u2;
 
-  function ensureUsers(done) {
-    u1 = new User({ name: 'Alice' })
-    u2 = new User({ name: 'Roger' })
-    User.remove(() => {
-      User.create([u1, u2], done);
-    });
+  function ensureUsers() {
+    u1 = new User({ name: 'Alice' });
+    u2 = new User({ name: 'Roger' });
+
+    return User.remove()
+      .then(() => User.create([u1, u2]));
   }
 
-  setup(ensureUsers);
+  setup(() => {
+    return ensureUsers();
+  });
 
   suite('requesting friends', function() {
     suite('.requestFriend', function() {
-      setup(function(done) {
-        User.requestFriend(u1, u2)
-          .then(done);
+      setup(function() {
+        return User.requestFriend(u1, u2);
       });
+
       // test the basic behavior
       requestFriendBehavior();
 
-      test('request by requested should accept friendship on both sides', function(done) {
-        User.requestFriend(u2, u1, function(err, fships) {
-          fships.friender.status.should.eql(Status.Accepted);
-
-          User.findById(u1._id, pathName, function(err, doc) {
+      test('request by requested should accept friendship on both sides', function() {
+        return User.requestFriend(u2, u1)
+          .then((fships) => {
+            fships.friender.status.should.eql(Status.Accepted);
+          })
+          .then(() => User.findById(u1._id, pathName))
+          .then((doc) => {
             doc[pathName].id(u2.id).status.should.eql(Status.Accepted);
-
-            User.findById(u2._id, pathName, function(err, doc) {
-              doc[pathName].id(u1.id).status.should.eql(Status.Accepted);
-              done();
-            });
+          })
+          .then(() => User.findById(u2._id, pathName))
+          .then((doc) => {
+            doc[pathName].id(u1.id).status.should.eql(Status.Accepted);
           });
-        });
       });
 
-      test('requesting a 2nd time should have no effect', function(done) {
-        User.requestFriend(u1, u2, function(err, fships) {
-          fships.friender.status.should.eql(Status.Requested);
-          User.findById(u2._id, pathName, function(err, doc) {
+      test('requesting a 2nd time should have no effect', function() {
+        return User.requestFriend(u1, u2)
+          .then((fships) => {
+            fships.friender.status.should.eql(Status.Requested);
+          })
+          .then(() => User.findById(u2._id, pathName))
+          .then((doc) => {
             doc[pathName].length.should.eql(1);
             doc[pathName].id(u1._id).status.should.eql(Status.Pending);
-            done();
-          });
-        });
+          })
       });
 
       suite('when requestee has already accepted', function() {
-        setup(function(done) {
+        setup(function() {
           var query = {_id: u2._id};
           query[pathName] = {$elemMatch: {_id: u1._id}};
 
           var update = {$set: {}};
           update.$set[pathName+'.$.status'] = Status.Accepted;
 
-          User.findOneAndUpdate(query, update, done);
+          return User.findOneAndUpdate(query, update);
         });
 
-        test('re-requesting should accept friendship on both sides', function(done) {
-          User.requestFriend(u1, u2, function(err, fships) {
-            fships.friender.status.should.eql(Status.Accepted);
-
-            User.findById(u1._id, pathName, function(err, doc) {
+        test('re-requesting should accept friendship on both sides', function() {
+          return User.requestFriend(u1, u2)
+            .then((fships) => {
+              fships.friender.status.should.eql(Status.Accepted);
+            })
+            .then(() => User.findById(u1._id, pathName))
+            .then((doc) => {
               doc[pathName].id(u2.id).status.should.eql(Status.Accepted);
-              User.findById(u2._id, pathName, function(err, doc) {
-                doc[pathName].id(u1.id).status.should.eql(Status.Accepted);
-                done();
-              });
+            })
+            .then(() => User.findById(u2._id, pathName))
+            .then((doc) => {
+              doc[pathName].id(u1.id).status.should.eql(Status.Accepted);
             });
-          });
         });
       });
 
       suite('when requestee has requested requester', function() {
-        setup(function(done) {
+        setup(function() {
           var query = {_id: u2._id};
           query[pathName] = {$elemMatch: {_id: u1._id}};
 
           var update = {$set: {}};
           update.$set[pathName+'.$.status'] = Status.Requested;
 
-          User.findOneAndUpdate(query, update, done);
+          return User.findOneAndUpdate(query, update);
         });
 
-        test('re-requesting should accept friendship on both sides', function(done) {
-          User.requestFriend(u1, u2, function(err, fships) {
-            fships.friender.status.should.eql(Status.Accepted);
-
-            User.findById(u1._id, pathName, function(err, doc) {
+        test('re-requesting should accept friendship on both sides', function() {
+          return User.requestFriend(u1, u2)
+            .then((fships) => {
+              fships.friender.status.should.eql(Status.Accepted);
+            })
+            .then(() => User.findById(u1._id, pathName))
+            .then((doc) => {
               doc[pathName].id(u2.id).status.should.eql(Status.Accepted);
-              User.findById(u2._id, pathName, function(err, doc) {
-                doc[pathName].id(u1.id).status.should.eql(Status.Accepted);
-                done();
-              });
+            })
+            .then(() => User.findById(u2._id, pathName))
+            .then((doc) => {
+              doc[pathName].id(u1.id).status.should.eql(Status.Accepted);
             });
-          });
         });
       });
     });
 
     suite('#requestFriend', function() {
-      setup(function(done) {
-        u1.requestFriend(u2, done);
+      setup(function() {
+        return u1.requestFriend(u2);
       });
+
       requestFriendBehavior();
     });
 
     function requestFriendBehavior () {
-      test('requester should have requested friend request', function(done) {
-        User.findById(u1._id, pathName, function(err, doc) {
-          doc[pathName].id(u2.id).status.should.eql(Status.Requested);
-          done();
-        });
+      test('requester should have requested friend request', function() {
+        return User.findById(u1._id, pathName)
+          .then((doc) => {
+            doc[pathName].id(u2.id).status.should.eql(Status.Requested);
+          });
       });
 
-      test('requestee should have pending friend request', function(done) {
-        User.findById(u2._id, pathName, function(err, doc) {
-          doc[pathName].id(u1.id).status.should.eql(Status.Pending);
-          done();
-        });
+      test('requestee should have pending friend request', function() {
+        return User.findById(u2._id, pathName)
+          .then((doc) => {
+            doc[pathName].id(u1.id).status.should.eql(Status.Pending);
+          });
       });
     }
   });
 
   suite('getting friends', function() {
-
-    setup(function(done) {
-      User.requestFriend(u1, u2, done);
+    setup(function() {
+      return User.requestFriend(u1, u2);
     });
 
     suite('.getFriends', function() {
@@ -164,19 +170,20 @@ suite('friends', function() {
 
     suite('status helpers', function() {
       function check(type, user, len, instance) {
-        return function(done) {
+        return function() {
           user = user ? u1 : u2;
 
-          var cb = function(err, f) {
-            f.length.should.eql(len);
-            done();
-          }
-
           if (instance) {
-            user['get'+type+'Friends'](cb);
+            return user[`get${type}Friends`]()
+              .then((friends) => {
+                friends.length.should.eql(len);
+              });
           }
           else {
-            User['get'+type+'Friends'](user, cb);
+            return User[`get${type}Friends`](user)
+              .then((friends) => {
+                friends.length.should.eql(len);
+              });
           }
         }
       }
@@ -209,8 +216,8 @@ suite('friends', function() {
       });
 
       suite('after reciprocation', function() {
-        setup(function(done) {
-          User.requestFriend(u2, u1, done);
+        setup(function() {
+          return User.requestFriend(u2, u1);
         });
         suite('.getPendingFriends', function() {
           test('requester should have 0', check('Pending', 1, 0));
@@ -240,103 +247,117 @@ suite('friends', function() {
     });
 
     suite('sorting & limiting', function() {
-      var u3, u4;
+      let u3, u4, u5, u6;
 
-      var reciprocate = function(a, b) {
-        return function(done) {
-          a.requestFriend(b, function() {
-            b.requestFriend(a, done);
+      const reciprocate = function(a, b) {
+        return a.requestFriend(b)
+          .then(() => b.requestFriend(a));
+      }
+
+      const request = function(a, b) {
+        return a.requestFriend(b);
+      }
+
+      setup(function() {
+        u3 = new User({ name: 'Zeke' });
+        u4 = new User({ name: 'Beatrice' });
+        u5 = new User({ name: 'Dan' });
+        u6 = new User({ name: 'Norm' });
+
+        return User.create([u3, u4, u5, u6])
+          .then(() => {
+            return Promise.all([
+              reciprocate(u1, u2),
+              reciprocate(u1, u3),
+              reciprocate(u1, u4),
+              request(u1, u5),
+              request(u1, u6),
+              reciprocate(u5, u6),
+              request(u5, u4),
+            ]);
           })
-        }
-      }
-
-      var request = function(a, b) {
-        return function(done) {
-          a.requestFriend(b, done);
-        }
-      }
-
-      setup(function(done) {
-        u3 = new User({name: 'Zeke'});
-        u4 = new User({name: 'Beatrice'});
-        u5 = new User({name: 'Dan'});
-        u6 = new User({name: 'Norm'});
-        User.create([u3, u4, u5, u6], function() {
-          async.parallel([
-            reciprocate(u1, u2),
-            reciprocate(u1, u3),
-            reciprocate(u1, u4),
-            request(u1, u5),
-            request(u1, u6),
-            reciprocate(u5, u6),
-            request(u5, u4)
-          ], function() {
-            User.getFriends(u1, function(err, friends) {
-              // sanity
-              friends.length.should.eql(5);
-              done();
-            });
+          .then((friends) => {
+            return User.getFriends(u1)
+              .then((friends) => {
+                friends.length.should.eql(5);
+              });
           });
-        });
       });
 
-      test('status condition (Accepted)', function(done) {
-        var conditions = {};
-        conditions[pathName+'.status'] = Status.Accepted;
+      test('status condition (Accepted)', function() {
+        const conditions = {
+          [`${pathName}.status`]: Status.Accepted,
+        };
 
-        u1.getFriends(conditions, function(err, friends) {
-          friends.length.should.eql(3);
-          done();
-        });
+        return u1.getFriends({ conditions })
+          .then((friends) => {
+            friends.length.should.eql(3);
+          });
       });
 
-      test('status condition (Requested)', function(done) {
-        var conditions = {};
-        conditions[pathName+'.status'] = Status.Requested;
+      test('status condition (Requested)', function() {
+        const conditions = {
+          [`${pathName}.status`]: Status.Requested,
+        };
 
-        u1.getFriends(conditions, function(err, friends) {
-          friends.length.should.eql(2);
-          done();
-        });
+        return u1.getFriends({ conditions })
+          .then((friends) => {
+            friends.length.should.eql(2);
+          });
       });
 
-      test('select fields', function(done) {
-        u1.getFriends({_id: u6._id}, {_id: 1}, function(err, friends) {
-          should.not.exist(friends[0].friend.name);
-          friends[0].friend._id.should.eql(u6._id);
-          done();
-        });
+      test('select fields', function() {
+        return u1.getFriends({
+          conditions: {_id: u6._id},
+          fields: { _id: 1 },
+        })
+          .then((friends) => {
+            should.not.exist(friends[0].friend.name);
+            friends[0].friend._id.should.eql(u6._id);
+          });
       });
 
-      test('limiting', function(done) {
-        User.getFriends(u1, {}, null, {limit: 2}, function(err, friends) {
-          friends.length.should.eql(2);
-          done();
-        });
+      test('limiting', function() {
+        return User.getFriends(u1, {
+          conditions: {},
+          fields: null,
+          options: { limit: 2 },
+        })
+          .then((friends) => {
+            friends.length.should.eql(2);
+          });
       });
 
-      test('sorting', function(done) {
-        var names = [u2, u3, u4, u5, u6].map(function(u) { return u.name; }).sort();
-        User.getFriends(u1, {}, null, {sort: {name: 1}}, function(err, friends) {
-          assert.deepEqual(names, friends.map(function(fship) {
-            return fship.friend.name;
-          }));
-          done();
-        });
+      test('sorting', function() {
+        const names = [u2, u3, u4, u5, u6].map((u) => u.name).sort();
+
+        return User.getFriends(u1, {
+          conditions: {},
+          fields: null,
+          options: { sort: { name: 1 } },
+        })
+          .then((friends) => {
+            assert.deepEqual(names, friends.map((fship) => fship.friend.name));
+          });
       });
     });
 
     function getFriendBehavior(isStatic) {
-      var shouldHave = function(friend, status, user2) {
-        return function(done) {
-          var user, other;
+      const shouldHave = function(friend, status, user2) {
+        return function() {
+          let user;
+          let other;
+
           if (user2) {
-            user = u2; other = u1;
+            user = u2;
+            other = u1;
           }
           else {
-            user = u1; other = u2;
+            user = u1;
+            other = u2;
           }
-          var cb = function(err, friends) {
+
+          const cb = (friends) => {
             if (friend) {
               friends.length.should.eql(1);
               friends[0].friend._id.should.eql(other._id);
@@ -344,17 +365,23 @@ suite('friends', function() {
             else {
               friends.length.should.eql(0);
             }
-            done();
           }
 
-          var conditions = {};
-          conditions[pathName+'.status'] = status;
+          const conditions = {
+            [`${pathName}.status`]: status,
+          };
 
           if (isStatic) {
-            User.getFriends(user, conditions, null, {sort: {name: 1}}, cb)
+            return User.getFriends(user, {
+              conditions,
+              fields: null,
+              options: { sort: { name: 1 } },
+            })
+              .then((friends) => cb(friends));
           }
           else {
-            user.getFriends(conditions, cb);
+            return user.getFriends({ conditions })
+              .then((friends) => cb(friends));
           }
         }
       }
@@ -374,8 +401,8 @@ suite('friends', function() {
       });
 
       suite('after request accepted', function() {
-        setup(function(done) {
-          User.requestFriend(u2, u1, done);
+        setup(function() {
+          return User.requestFriend(u2, u1);
         });
 
         suite('requester', function() {
@@ -390,84 +417,84 @@ suite('friends', function() {
   });
 
   suite('removing friends', function() {
-    setup(function(done) {
-      ensureUsers(function() {
-        User.requestFriend(u1, u2, function() {
-          User.requestFriend(u2, u1, done);
-        });
-      });
+    setup(function() {
+      return ensureUsers()
+        .then(() => User.requestFriend(u1, u2))
+        .then(() => User.requestFriend(u2, u1));
     });
 
     suite('.removeFriend', function() {
-      setup(function(done) {
-        User.removeFriend(u1, u2, done);
+      setup(function() {
+        return User.removeFriend(u1, u2);
       });
+
       removeFriendBehavior();
     });
 
     suite('#removeFriend', function() {
-      setup(function(done) {
-        u1.removeFriend(u2, done);
+      setup(function() {
+        return u1.removeFriend(u2);
       });
+
       removeFriendBehavior();
     });
 
     function removeFriendBehavior() {
-      test('remover should have no friendship', function(done) {
-        User.getFriends(u1, function(err, friends) {
-          friends.length.should.eql(0);
-          done();
-        });
+      test('remover should have no friendship', function() {
+        return User.getFriends(u1)
+          .then((friends) => {
+            friends.length.should.eql(0);
+          });
       });
 
-      test('removee should no longer have friendship', function(done) {
-        User.getFriends(u2, function(err, friends) {
-          friends.length.should.eql(0);
-          done();
-        });
+      test('removee should no longer have friendship', function() {
+        return User.getFriends(u2)
+          .then((friends) => {
+            friends.length.should.eql(0);
+          });
       });
     }
   });
 
   suite('checking friendship', function() {
-    test('should not be friends', function(done) {
-      checkFriendship(false, done);
+    test('should not be friends', function() {
+      return checkFriendship(false);
     });
 
     suite('after requesting', function() {
-      setup(function(done) {
-        User.requestFriend(u1, u2, done);
+      setup(function() {
+        return User.requestFriend(u1, u2);
       });
 
-      test('should not be friends after requesting', function(done) {
-        checkFriendship(false, done);
+      test('should not be friends after requesting', function() {
+        return checkFriendship(false);
       });
     });
 
     suite('after accepting', function() {
-      setup(function(done) {
-        User.requestFriend(u1, u2, () => {
-          User.requestFriend(u2, u1, done);
-        });
+      setup(function() {
+        return User.requestFriend(u1, u2)
+          .then(() => User.requestFriend(u2, u1));
       });
 
-      test('should be friends after accepting', function(done) {
-        checkFriendship(true, done);
+      test('should be friends after accepting', function() {
+        return checkFriendship(true);
       });
     });
 
-    function checkFriendship(shouldBeFriends, done) {
-      User.areFriends(u1, u2, (areFriends) => {
-        areFriends.should.eql(shouldBeFriends);
-
-        u1.isFriendsWith(u2, (isFriend) => {
+    function checkFriendship(shouldBeFriends) {
+      return User.areFriends(u1, u2)
+        .then((areFriends) => {
+          areFriends.should.eql(shouldBeFriends);
+        })
+        .then(() => u1.isFriendsWith(u2))
+        .then((isFriend) => {
           isFriend.should.eql(shouldBeFriends);
-          u2.isFriendsWith(u1, (isFriend2) => {
-            isFriend2.should.eql(shouldBeFriends);
-            done();
-          });
+        })
+        .then(() => u2.isFriendsWith(u1))
+        .then((isFriend2) => {
+          isFriend2.should.eql(shouldBeFriends);
         });
-      });
     }
   });
 
