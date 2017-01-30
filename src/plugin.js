a28a9462e19ed0b2d18652de6a68450124904c72
Martin Litvaj <kamahl19@gmail.com>
2017-01-30 16:40:35 +0100

    remove unnecessary check

--- src/plugin.js
+++ src/plugin.js
@@ -136,7 +136,6 @@ function friendsPlugin(options) {
         .then(([m1Res, m2Res]) => {
           const hasfship = !!m1Res;
           const fship = m1Res || { _id: m2 };
-          const oid = fship._id;
           const ostatus = fship.status;
 
           const steps = [];
@@ -177,7 +176,7 @@ function friendsPlugin(options) {
           }
 
           // If no update was necessary, send the local friendship back directly
-          if (hasfship && ostatus === fship.status && oid.equals(fship._id)) {
+          if (hasfship && ostatus === fship.status) {
             steps[1] = Promise.resolve(fship);
           }
           // Otherwise update it
